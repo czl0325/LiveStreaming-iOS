980a5909c1daf2316869a17ee267104e04123e43
czl <czl2106082@163.com>
2019-04-03 13:00:32 +0800

    完善demo1

--- 01-摄像头采集数据写入文件/01-摄像头采集数据写入文件/ViewController.m
+++ 01-摄像头采集数据写入文件/01-摄像头采集数据写入文件/ViewController.m
@@ -10,17 +10,17 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
-<AVCaptureVideoDataOutputSampleBufferDelegate,AVCaptureAudioDataOutputSampleBufferDelegate>
+<AVCaptureVideoDataOutputSampleBufferDelegate,AVCaptureAudioDataOutputSampleBufferDelegate,AVCaptureFileOutputRecordingDelegate>
 
 //捕捉画面的session
 @property(nonatomic,strong)AVCaptureSession* session;
-//
+//视频文件输出的uoutput
 @property(nonatomic,strong)AVCaptureVideoDataOutput* videoOutput;
 //展示预览图层的layer
 @property(nonatomic,strong)AVCaptureVideoPreviewLayer* previewLayer;
-//
+//视频文件输入的intput
 @property(nonatomic,strong)AVCaptureDeviceInput* videoInput;
-//
+//视频文件输出的output
 @property(nonatomic,strong)AVCaptureMovieFileOutput* movieOutput;
 
 @end
@@ -73,6 +73,7 @@
     [self.session commitConfiguration];
 }
 
+//初始化音频输入输出流
 - (void)setupAudioIntputOutput {
     AVCaptureDevice* device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeAudio];
     if (device == nil) {
@@ -96,6 +97,7 @@
     [self.session commitConfiguration];
 }
 
+//初始化预览图层
 - (void)setupPreviewLayer {
     AVCaptureVideoPreviewLayer* layer = [[AVCaptureVideoPreviewLayer alloc]initWithSession:self.session];
     if (layer == nil) {
@@ -106,29 +108,99 @@
     self.previewLayer = layer;
 }
 
+//创建视频文件写入的movieOutput
+- (void)setupMovieFileOutput {
+    if (self.movieOutput) {
+        [self.session removeOutput:self.movieOutput];
+    }
+    
+    AVCaptureMovieFileOutput* movieOutput = [[AVCaptureMovieFileOutput alloc]init];
+    self.movieOutput = movieOutput ;
+
+    AVCaptureConnection *connect = [movieOutput connectionWithMediaType:AVMediaTypeVideo];
+    connect.automaticallyAdjustsVideoMirroring = YES;
+    connect.videoOrientation = AVCaptureVideoOrientationPortrait;
+    
+    if ([self.session canAddOutput:movieOutput]) {
+        [self.session addOutput:movieOutput];
+        
+        //视频文件的写入
+        NSString *filePath = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES)lastObject] stringByAppendingPathComponent:@"test.mp4"];
+        
+        [movieOutput startRecordingToOutputFileURL:[NSURL fileURLWithPath:filePath] recordingDelegate:self];
+    }
+}
+
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
     if (connection == [self.videoOutput connectionWithMediaType:AVMediaTypeVideo]) {
-        NSLog(@"采集到视频");
+        //NSLog(@"采集到视频");
     } else {
-        NSLog(@"采集到音频");
+        //NSLog(@"采集到音频");
     }
+}
+
+- (void)captureOutput:(AVCaptureFileOutput *)output didStartRecordingToOutputFileAtURL:(NSURL *)fileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections {
+    NSLog(@"开始写入视频文件");
+}
+
+- (void)captureOutput:(AVCaptureFileOutput *)output didFinishRecordingToOutputFileAtURL:(NSURL *)outputFileURL fromConnections:(NSArray<AVCaptureConnection *> *)connections error:(nullable NSError *)error {
+    NSLog(@"结束写入视频文件");
 }
 
 
 - (IBAction)startCapture:(id)sender {
+    if (self.session.isRunning) {
+        return;
+    }
     [self.session startRunning];
     
     [self setupPreviewLayer];
+    
+    [self setupMovieFileOutput];
 }
 
 - (IBAction)stopCapture:(id)sender {
     [self.session stopRunning];
     
     [self.previewLayer removeFromSuperlayer];
+    
+    [self.movieOutput stopRecording];
 }
 
 - (IBAction)switchCamera:(id)sender {
-    
+    if (self.videoInput != nil) {
+        //先获取原来的摄像头反向，转换摄像头
+        AVCaptureDevicePosition position = self.videoInput.device.position == AVCaptureDevicePositionBack ? AVCaptureDevicePositionFront : AVCaptureDevicePositionBack;
+        
+        //重新设置一个新的输入源
+        NSArray<AVCaptureDevice*>* devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+        if (devices.count < 1) {
+            return;
+        }
+        __block AVCaptureDevice* device = nil;
+        [devices enumerateObjectsUsingBlock:^(AVCaptureDevice * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            if (obj.position == position) {
+                device = obj;
+                *stop = YES;
+            }
+        }];
+        if (device == nil) {
+            return;
+        }
+        AVCaptureDeviceInput* newInput = [[AVCaptureDeviceInput alloc]initWithDevice:device error:nil];
+        if (newInput == nil) {
+            return;
+        }
+        
+        //移除旧的输入源，添加新的输入源
+        [self.session beginConfiguration];
+        [self.session removeInput:self.videoInput];
+        if ([self.session canAddInput:newInput]) {
+            [self.session addInput:newInput];
+        }
+        [self.session commitConfiguration];
+        self.videoInput = newInput;
+    }
 }
 
 - (AVCaptureSession*)session {
